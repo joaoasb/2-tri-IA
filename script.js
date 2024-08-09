d17152c2d5ac56657c3e0c90d3b5492513b53fa5
joaoasb <114587403+joaoasb@users.noreply.github.com>
2024-08-08 21:38:36 -0300

    08-08

--- script.js
+++ script.js
@@ -6,70 +6,71 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Você está vendo um novo modelo de caminhão em uma feira de veículos. Qual é o seu primeiro pensamento?",
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-                texto: "Isso é incrível! Adoro ver as inovações no setor de caminhões.",
-                afirmacao: "Ficou animado com as novas tecnologias e tendências em caminhões."
+                texto: "Isso é assustador!",
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
-                texto: "Isso parece um exagero. Não vejo necessidade de tantas mudanças.",
-                afirmacao: "Sentiu que a inovação pode ser excessiva e desnecessária."
+                texto: "Isso é maravilhoso!",
+                afirmacao: "Quis saber como usar IA no seu dia a dia."
             }
         ]
     },
     {
-        enunciado: "Um fabricante de caminhões pede que você escreva um relatório sobre a importância das tecnologias avançadas nos caminhões. Qual é a sua abordagem?",
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
             {
-                texto: "Utilizo informações de sites especializados e explico como a tecnologia pode melhorar a eficiência dos caminhões.",
-                afirmacao: "Focou em mostrar os benefícios práticos das tecnologias avançadas em caminhões."
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
             },
             {
-                texto: "Escrevo o relatório com base em conversas com colegas e minhas próprias observações sobre caminhões.",
-                afirmacao: "Prefere utilizar conhecimentos pessoais e experiências para elaborar o relatório."
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
             }
         ]
     },
     {
-        enunciado: "Durante uma discussão sobre o futuro dos caminhões, você é questionado sobre como as tecnologias impactarão o setor. Como você se posiciona?",
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
         alternativas: [
             {
-                texto: "Acredito que a tecnologia pode trazer grandes melhorias para o setor, como caminhões mais seguros e econômicos.",
-                afirmacao: "Vê as tecnologias como uma oportunidade para revolucionar o setor de caminhões."
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
             },
             {
-                texto: "Estou preocupado com a substituição dos trabalhadores por tecnologias e acho que devemos proteger os empregos no setor.",
-                afirmacao: "Preocupa-se com o impacto das tecnologias na força de trabalho e defende a proteção dos empregos."
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
             }
         ]
     },
     {
-        enunciado: "Você precisa criar uma apresentação sobre os diferentes tipos de caminhões para um evento. Qual é o seu método?",
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
         alternativas: [
             {
-                texto: "Crio uma apresentação utilizando um software de design para criar slides detalhados.",
-                afirmacao: "Usa ferramentas tradicionais para criar uma apresentação visualmente atraente sobre caminhões."
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
             },
             {
-                texto: "Utilizo um gerador de apresentações automatizado para acelerar o processo.",
-                afirmacao: "Prefere usar tecnologias automatizadas para tornar o processo mais eficiente."
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
             }
         ]
     },
     {
-        enunciado: "Seu grupo de trabalho está atrasado na entrega de um projeto sobre caminhões e alguém decidiu usar um gerador de conteúdo para ajudar. O que você faz?",
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
         alternativas: [
             {
-                texto: "Não vejo problema em usar o conteúdo gerado, pois é uma forma eficiente de concluir o projeto.",
-                afirmacao: "Adota uma abordagem mais prática e se acostuma a depender das tecnologias automatizadas."
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
             },
             {
-                texto: "Reviso o conteúdo gerado e adiciono minhas próprias ideias para garantir que o trabalho reflita nosso ponto de vista.",
-                afirmacao: "Entende que a tecnologia deve ser usada como auxílio, mas o trabalho final deve refletir o esforço e a perspectiva do grupo."
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
             }
         ]
     },
+];
 
 
 let atual = 0;
